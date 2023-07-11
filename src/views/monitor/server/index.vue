c4e0dd71f57aa41a4ab4fa663db574bab27032f5
Wu Clan <jianhengwu0407@gmail.com>
2023-07-11 18:24:59 +0800

    chore: add loading animations

--- src/views/monitor/server/index.vue
+++ src/views/monitor/server/index.vue
@@ -6,45 +6,46 @@
         <div class="content">
           <a-row :gutter="20">
             <a-col :span="12">
-              <a-card title="CPU" class="info-card">
+              <a-card :loading="loading" class="info-card" title="CPU">
                 <a-space :size="60">
                   <template #split>
                     <a-divider direction="vertical" />
                   </template>
                   <a-statistic
-                    :extra="$t('menu.monitor.server.cpu.usage')"
-                    :value="cpuData.usage"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :animation="true"
+                    :extra="$t('menu.monitor.server.cpu.usage')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
+                    :value="cpuData.usage"
                     :value-style="usageStyle()"
                   >
                     <template #suffix> %</template>
                   </a-statistic>
                   <a-statistic
-                    :extra="$t('menu.monitor.server.cpu.current-frequency')"
-                    :value="cpuData.current_freq"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :animation="true"
+                    :extra="$t('menu.monitor.server.cpu.current-frequency')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
+                    :value="cpuData.current_freq"
                   >
                     <template #suffix> MHz</template>
                   </a-statistic>
                   <a-statistic
-                    :extra="$t('menu.monitor.server.cpu.logical-cores')"
-                    :value="cpuData.logical_num"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :animation="true"
+                    :extra="$t('menu.monitor.server.cpu.logical-cores')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
+                    :value="cpuData.logical_num"
                   />
                   <a-statistic
-                    :extra="$t('menu.monitor.server.cpu.physical-cores')"
-                    :value="cpuData.physical_num"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :animation="true"
+                    :extra="$t('menu.monitor.server.cpu.physical-cores')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
+                    :value="cpuData.physical_num"
                   />
                 </a-space>
               </a-card>
             </a-col>
             <a-col :span="12">
               <a-card
+                :loading="loading"
                 :title="$t('menu.monitor.server.memory')"
                 class="info-card"
               >
@@ -53,34 +54,34 @@
                     <a-divider direction="vertical" />
                   </template>
                   <a-statistic
+                    :animation="true"
                     :extra="$t('menu.monitor.server.memory.total')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :value="memData.total"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
-                    :animation="true"
                   >
                     <template #suffix> GB</template>
                   </a-statistic>
                   <a-statistic
+                    :animation="true"
                     :extra="$t('menu.monitor.server.memory.used')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :value="memData.used"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
-                    :animation="true"
                   >
                     <template #suffix> GB</template>
                   </a-statistic>
                   <a-statistic
+                    :animation="true"
                     :extra="$t('menu.monitor.server.memory.free')"
-                    :value="memData.free"
                     :placeholder="$t('menu.monitor.server.placeholder')"
-                    :animation="true"
+                    :value="memData.free"
                   >
                     <template #suffix> GB</template>
                   </a-statistic>
                   <a-statistic
-                    :extra="$t('menu.monitor.server.memory.usage')"
-                    :value="memData.usage"
-                    :placeholder="$t('menu.monitor.server.placeholder')"
                     :animation="true"
+                    :extra="$t('menu.monitor.server.memory.usage')"
+                    :placeholder="$t('menu.monitor.server.placeholder')"
+                    :value="memData.usage"
                     :value-style="usageStyle()"
                   >
                     <template #suffix> %</template>
@@ -90,21 +91,33 @@
             </a-col>
           </a-row>
           <a-space style="padding-top: 22px" />
-          <a-card :title="$t('menu.monitor.server.service')" class="info-card">
+          <a-card
+            :loading="loading"
+            :title="$t('menu.monitor.server.service')"
+            class="info-card"
+          >
             <a-descriptions :data="serviceData"></a-descriptions>
           </a-card>
           <a-space style="padding-top: 22px" />
-          <a-card :title="$t('menu.monitor.server.system')" class="info-card">
+          <a-card
+            :loading="loading"
+            :title="$t('menu.monitor.server.system')"
+            class="info-card"
+          >
             <a-descriptions
-              :data="osData"
-              :column="4"
-              :layout="'vertical'"
               :bordered="true"
+              :column="4"
+              :data="osData"
+              :layout="'vertical'"
             ></a-descriptions>
           </a-card>
           <a-space style="padding-top: 22px" />
-          <a-card :title="$t('menu.monitor.server.disk')" class="info-card">
-            <a-descriptions :data="diskData" :bordered="true"></a-descriptions>
+          <a-card
+            :loading="loading"
+            :title="$t('menu.monitor.server.disk')"
+            class="info-card"
+          >
+            <a-descriptions :bordered="true" :data="diskData"></a-descriptions>
           </a-card>
         </div>
       </a-card>
@@ -123,7 +136,7 @@
   import { computed, ref } from 'vue';
   import { DescData } from '@arco-design/web-vue';
 
-  const { setLoading } = useLoading(true);
+  const { loading, setLoading } = useLoading(true);
 
   const setServerData = ref<Record<string, any>>({});
   const cpuData = computed(() => {
